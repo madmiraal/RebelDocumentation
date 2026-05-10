fd28eac501af90a8d1bcd650551ad7c4daad36c4
Marcel Admiraal <madmiraal@users.noreply.github.com>
2026-05-10 12:00:16 +0200

    Update translations for Rebel

--- conf.py
+++ conf.py
@@ -64,12 +64,7 @@ highlight_language = "gdscript"
 
 
 # 3. Options for internationalization
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-internationalization
-
-# TODO Rebel internationalisation needs to configured.
-# https://www.sphinx-doc.org/en/master/usage/advanced/intl.html
-# https://readthedocs-lst.readthedocs.io/en/latest/i18n.html
-# https://readthedocs-lst.readthedocs.io/en/latest/localization.html
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-internationalisation
 
 supported_languages = {
     "en": "Rebel Toolbox (%s) documentation in English",
